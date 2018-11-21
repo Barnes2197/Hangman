12ae43ba46e90fc9fd44b3fda286af003312165d
Maurice Barnes <mauriceb2197@gmail.com>
2018-11-20 20:37:36 -0500

    Functioning program

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,20 +1,18 @@
 package hangman;
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class Hangman {
 
-    
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         new Hangman();
     }
-    
-    public Hangman()
-    {
+
+    public Hangman() {
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
-        String words[] = {"game", "illustration","java", "class", "albatross"};
+        String words[] = {"game", "illustration", "java", "class", "albatross"};
         int pickWord = rnd.nextInt(words.length);
         String question = words[pickWord];
         boolean game[] = new boolean[question.length()];
@@ -22,102 +20,54 @@ public class Hangman {
         String question = words[pickWord];
         boolean game[] = new boolean[question.length()];
         boolean gameOver = false;*/
-                       
-        for(int i = 0; i < game.length;i++)
-        {
+
+        for (int i = 0; i < game.length; i++) {
             game[i] = false;
         }
+
         int counter = 0;
         int winCounter = 0;
-        String hangman;
-        
-        while(counter < 9){
-            
-           switch(counter)
-        {
-            case 1:
-                hangman = "\n" + "\n|" + "\n|" + "\n|" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 2:
-                hangman = "\n_________" + "\n|" + "\n|" + "\n|" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 3:
-                hangman =  "\n_________" + "\n|                   |" + "\n|" + "\n|" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 4:
-                hangman =  "\n_________" + "\n|                   |" + "\n|                  O" + "\n|" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 5:
-                hangman =  "\n_________" + "\n|                   |" + "\n|                  O" + "\n|                   |" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 6:
-                hangman =  "\n_________" + "\n|                   |" + "\n|                  O" + "\n|               ---|" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 7:
-                hangman =  "\n_________" + "\n|                   |" + "\n|                  O" + "\n|               ---|---" + "\n|" + "\n|" + "\n|_______________________\n";
-                break;
-            case 8:
-                hangman = "\n_________" + "\n|                   |" + "\n|                  O" + "\n|               ---|---" + "\n|                  /" + "\n|                /" + "\n|_______________________\n";
-                break;
-            case 9:
-                hangman = ("Incorrect" + "\n_________" + "\n|                   "
-                        + "|" + "\n|                  O" + "\n|               "
-                        + "---|---" + "\n|                  /\\" + "\n|      "
-                        + "          /    \\" + "\n|____________________"
-                        + "___" + "\nSorry, you lost.\nThe stick man is Dead!"
-                        + "\nThe answer word was: \"" + question + "\"");
-                
-                                                          
-            
-            for(int i  = 0; i < game.length;i++)
-            {
-                if(game[i] == true)
+
+        while (counter < 5) {
+            //Displaying empty spaces for so you know the number of spaces
+            for (int i = 0; i < game.length; i++) {
+                if (game[i] == true) {
                     System.out.print(question.charAt(i));
-                else
+                } else {
                     System.out.print("_" + " ");
-                
+                }
+
             }
-            
+
             System.out.println("");
-            
+
             System.out.println("Enter a letter: ");
             char guess = in.next().charAt(0);
-            
-            for(int i = 0; i < game.length;i++)
-            {
-               if(guess == question.charAt(i))
-               {
-                   game[i] = true;
-               }
+            //Used to set a letter to true if you guess it right
+            for (int i = 0; i < game.length; i++) {
+                if (guess == question.charAt(i)) {
+                    game[i] = true;
+                }
             }
-            
-            counter++;
-            winCounter = 0;
-            for(int i = 0; i < game.length;i++)
-            {
-                if(game[i] == true)
-                    winCounter++;
-            }
-            if(winCounter == game.length)
-                break;
-            
-           }
-        }
-        if(counter >= 9)
-            System.out.println("You ran out of guesses");
-        else
-            System.out.println("You won");
-        
-        
-        
-        
-        
-        
-        
-        
-        }   
-    }
-    
-    
-     
 
+            counter++;//Used to give the user 9 tries
+            winCounter = 0;//Sets the WinCounter to 0 so it won't start a higher number the next loop
+            for (int i = 0; i < game.length; i++) {
+                if (game[i] == true) {
+                    winCounter++;
+                }
+            }
+            if (winCounter == game.length) {
+                break;
+            }
+
+        }
+        if (counter >= 5) {
+            System.out.println("You ran out of guesses");
+        } else {
+            System.out.println("You won");
+        }
+
+    }
+
+}
